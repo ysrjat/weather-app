366336d218a0be18e38db3827787c07ad085c1c3
yatender <yatendersingh@gamil.com>
2023-12-20 01:10:08 +0530

    first commit

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 ```bash
 python manage.py runserver 0.0.0.0:8000
 ```
+# weather app 
--- labDjango/settings.py
+++ labDjango/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "weather",
+    
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = "labDjango.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR/'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
